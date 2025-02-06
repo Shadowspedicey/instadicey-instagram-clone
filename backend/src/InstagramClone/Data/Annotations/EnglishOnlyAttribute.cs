515540714a8bc1f9686bfe308c4a6444d8613c4f
Ahmed Khaled <shadowspedicey@gmail.com>
2025-02-06 04:54:31 +0200

    Backend - Added an EnglishOnly custom DataAnnotation Attribute
    This verifies that a property contains only English letters

---
+++ backend/src/InstagramClone/Data/Annotations/EnglishOnlyAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace InstagramClone.Data.Annotations
+{
+	public class EnglishOnlyAttribute : ValidationAttribute
+	{
+		protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+		{
+			if (value is string v && Regex.IsMatch(v, "/^[A-Za-z0-9]*$/"))
+				return ValidationResult.Success;
+			return new ValidationResult("Only English letters allowed");
+		}
+	}
+}
