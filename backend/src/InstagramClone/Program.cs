108b19610ff9898936267be27d49b34e93b8a5ab
Ahmed Khaled <shadowspedicey@gmail.com>
2025-02-07 13:55:28 +0200

    Backend - Added JWT authentication
    Created a service to handle JWT token generation (which will later handle authentication tasks as well), and token validation to the middleware as well

--- backend/src/InstagramClone/Program.cs
+++ backend/src/InstagramClone/Program.cs
@@ -1,19 +1,45 @@
 using InstagramClone.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var signingKeys = builder.Configuration.GetRequiredSection("Authentication:Schemes:Bearer:SigningKeys").GetChildren().ToList();
+var jwtValidationParameters = new TokenValidationParameters
+{
+	ValidateIssuer = false,
+	ValidateAudience = false,
+	ValidateIssuerSigningKey = true,
+	IssuerSigningKeys = new List<SecurityKey>
+	{
+		new SymmetricSecurityKey(Convert.FromBase64String(signingKeys[0]["Value"])),
+		new SymmetricSecurityKey(Convert.FromBase64String(signingKeys[1]["Value"]))
+	}
+};
+builder.Services.AddSingleton(jwtValidationParameters);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Main")));
+builder.Services.AddAuthentication("Bearer")
+	.AddJwtBearer(options =>
+	{
+		options.MapInboundClaims = false;
+		options.TokenValidationParameters = jwtValidationParameters;
+	});
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
-    app.MapOpenApi();
+	app.MapOpenApi();
 
 app.UseHttpsRedirection();
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
+
+app.MapGet("", () => "Hey").RequireAuthorization();
 
 app.Run();
---
+++ backend/src/InstagramClone/Services/AuthService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace InstagramClone.Services
+{
+	public class AuthService(IConfiguration configuration)
+	{
+		private readonly IConfiguration _configuration = configuration;
+
+		public string GenereateToken()
+		{
+			var securityKey = new SymmetricSecurityKey(Convert.FromBase64String(_configuration["Authentication:Schemes:Bearer:SigningKeys:1:Value"]));
+			var signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+			var secToken = new JwtSecurityToken
+			(
+				signingCredentials: signingCredentials,
+				claims:
+				[
+					new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+					new Claim(JwtRegisteredClaimNames.Iat, DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString(), ClaimValueTypes.Integer64)
+				],
+				expires: DateTime.Now.AddDays(1)
+			);
+
+			var tokenHandler = new JwtSecurityTokenHandler();
+			return tokenHandler.WriteToken(secToken);
+		}
+	}
+}
