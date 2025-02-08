c99fa31e211e8c4e6bf73c8c0715f085987f5d54
Ahmed Khaled <shadowspedicey@gmail.com>
2025-02-08 19:41:37 +0200

    Backend - Created a Username custom DataAnnotation attribute
    This is useful for validating input relating to the user's username

---
+++ backend/src/InstagramClone/DTOs/Authentication/UserRegisterDTO.cs
@@ -0,0 +1,16 @@
+﻿using InstagramClone.Data.Annotations;
+using System.ComponentModel.DataAnnotations;
+
+namespace InstagramClone.DTOs.Authentication
+{
+	public class UserRegisterDTO
+	{
+		[EmailAddress]
+		public required string Email { get; set; }
+		[StringLength(50)]
+		public required string RealName { get; set; }
+		[Username]
+		public required string Username { get; set; }
+		public required string Password { get; set; }
+	}
+}
---
+++ backend/src/InstagramClone/Data/Annotations/UsernameAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace InstagramClone.Data.Annotations
+{
+	public class UsernameAttribute : ValidationAttribute
+	{
+		protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+		{
+			if (value is string username && username.Length > 0 && username.Length <= 20 && Regex.IsMatch(username, "/^[A-Za-z0-9]*$/"))
+				return ValidationResult.Success;
+			else return new ValidationResult("Username has to be a string with 20 or less English characters");
+		}
+	}
+}
