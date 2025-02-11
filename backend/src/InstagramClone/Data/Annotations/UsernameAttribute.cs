732c490cd197d70dc06a1f46a162cfc1d75b87bf
Ahmed Khaled <shadowspedicey@gmail.com>
2025-02-11 23:05:11 +0200

    Bazkend - Fixed Username regex string
    The validation would fail for valid usernames without this

--- backend/src/InstagramClone/Data/Annotations/UsernameAttribute.cs
+++ backend/src/InstagramClone/Data/Annotations/UsernameAttribute.cs
@@ -7,7 +7,7 @@ namespace InstagramClone.Data.Annotations
 	{
 		protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
 		{
-			if (value is string username && username.Length > 0 && username.Length <= 20 && Regex.IsMatch(username, "/^[A-Za-z0-9]*$/"))
+			if (value is string username && Regex.IsMatch(username, "^[A-Za-z0-9]{1,20}$"))
 				return ValidationResult.Success;
 			else return new ValidationResult("Username has to be a string with 20 or less English characters");
 		}
