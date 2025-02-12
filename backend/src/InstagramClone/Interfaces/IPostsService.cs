d59a2817cff3f23b45a7920ad760250407f29b3a
Ahmed Khaled <shadowspedicey@gmail.com>
2025-02-12 18:05:28 +0200

    Backend - Created the interface for PostsService
    The service itself was also created but not implemented

---
+++ backend/src/InstagramClone/Interfaces/IPostsService.cs
@@ -0,0 +1,17 @@
+﻿using FluentResults;
+using InstagramClone.Data;
+using InstagramClone.Data.Entities;
+using InstagramClone.DTOs.Posts;
+using System.Security.Claims;
+
+namespace InstagramClone.Interfaces
+{
+	public interface IPostsService
+	{
+		public Task<Result> CreatePost(ClaimsPrincipal user, PostCreateDTO postDTO);
+		public Task<Result<Post>> GetPost(string postID);
+		public Task<Result> DeletePost(ClaimsPrincipal user, string postID);
+		public Task<Result<ICollection<Comment>>> GetPostsComments(string postID);
+
+	}
+}
---
+++ backend/src/InstagramClone/Services/PostsService.cs
@@ -0,0 +1,34 @@
+﻿using FluentResults;
+using InstagramClone.Data;
+using InstagramClone.Data.Entities;
+using InstagramClone.DTOs.Posts;
+using InstagramClone.Interfaces;
+using System.Security.Claims;
+
+namespace InstagramClone.Services
+{
+	public class PostsService(AppDbContext dbContext) : IPostsService
+	{
+		private readonly AppDbContext _dbContext = dbContext;
+
+		public Task<Result> CreatePost(ClaimsPrincipal user, PostCreateDTO postDTO)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<Result> DeletePost(ClaimsPrincipal user, string postID)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<Result<Post>> GetPost(string postID)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<Result<ICollection<Comment>>> GetPostsComments(string postID)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
