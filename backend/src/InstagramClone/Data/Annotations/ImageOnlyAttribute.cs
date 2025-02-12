c6c3a97af307b4a6756f9d327da7126614792360
Ahmed Khaled <shadowspedicey@gmail.com>
2025-02-12 21:32:06 +0200

    Backend - Created file custom DataAnnotation attributes
    These attributes are used to validate if the file is an image and if it fits within the max size allowed

---
+++ backend/src/InstagramClone/Data/Annotations/ImageOnlyAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace InstagramClone.Data.Annotations
+{
+	public class ImageOnlyAttribute : ValidationAttribute
+	{
+		private readonly string[] allowedExtensions = { ".png", ".jpg", "jpeg" };
+		protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+		{
+			if (
+					value is IFormFile file &&
+					allowedExtensions.Contains(Path.GetExtension(file.FileName)) &&
+					Regex.IsMatch(file.ContentType, "^image/")
+				)
+				return ValidationResult.Success;
+			else
+				return new ValidationResult("Photo field must be an image.");
+		}
+	}
+}
---
+++ backend/src/InstagramClone/Data/Annotations/MaxFileSizeAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InstagramClone.Data.Annotations
+{
+	public class MaxFileSizeAttribute(long maxFileSizeInMB) : ValidationAttribute
+	{
+		private readonly long _maxFileSizeInMB = maxFileSizeInMB * 1024 * 1024;
+		protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+		{
+			if (value is IFormFile file && file.Length <= _maxFileSizeInMB)
+				return ValidationResult.Success;
+			else
+				return new ValidationResult($"File is too big. (Max file size: {maxFileSizeInMB} MB)");
+		}
+	}
+}
