ffbbf573dd70ceb2e768de30400d3692b5634fd8
Ahmed Khaled <shadowspedicey@gmail.com>
2025-02-13 13:56:08 +0200

    Backend - Added file validation to the DTO

--- backend/src/InstagramClone/DTOs/Posts/PostCreateDTO.cs
+++ backend/src/InstagramClone/DTOs/Posts/PostCreateDTO.cs
@@ -1,8 +1,12 @@
-﻿namespace InstagramClone.DTOs.Posts
+﻿using InstagramClone.Data.Annotations;
+
+namespace InstagramClone.DTOs.Posts
 {
 	public class PostCreateDTO
 	{
 		public string? Caption { get; set; }
+		[ImageOnly]
+		[MaxFileSize(8)]
 		public required IFormFile Photo { get; set; }
 	}
 }
