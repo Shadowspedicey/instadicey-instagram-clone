39c1ffbade3c14c42d0459ac4f1877c7bbf2225f
Ahmed Khaled <shadowspedicey@gmail.com>
2025-02-13 14:02:44 +0200

    Backend - Added a file controller
    This controller exposes and endpoint where SPAs can request files with an encrypted path (This path is stored in DB on each upload)

---
+++ backend/src/InstagramClone/Controllers/FileController.cs
@@ -0,0 +1,28 @@
+﻿using InstagramClone.Interfaces;
+using InstagramClone.Utils;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InstagramClone.Controllers
+{
+	[Route("[controller]")]
+	[ApiController]
+	public class FileController(IConfiguration configuration, IFileService fileService) : ControllerBase
+	{
+		private readonly IFileService _fileService = fileService;
+
+		[HttpGet("{encryptedFilePath}")]
+		public async Task<IActionResult> GetFile(string encryptedFilePath)
+		{
+			var result = await _fileService.GetFile(encryptedFilePath);
+			if (!result.IsSuccess)
+				if (result.HasError(e => e.Message == "NotFound"))
+					return NotFound();
+				else
+					return Problem(statusCode: 400, detail: result.Errors?[0]?.Message);
+
+			var (fileStream, fileName) = result.Value;
+			string contentType = Helpers.Files.GetMimeTypeForFileExtension(fileName);
+			return new FileStreamResult(fileStream, contentType);
+		}
+	}
+}
