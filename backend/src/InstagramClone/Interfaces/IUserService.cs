364b8b9f70d4fdc0bc3ba54035308190fcffc351
Ahmed Khaled <shadowspedicey@gmail.com>
2025-02-15 00:43:14 +0200

    Backend - Added a cancellation token to the ChangeProfilePic method in the interface

--- backend/src/InstagramClone/Interfaces/IUserService.cs
+++ backend/src/InstagramClone/Interfaces/IUserService.cs
@@ -8,11 +8,11 @@ namespace InstagramClone.Interfaces
 	public interface IUserService
 	{
 		public Task<Result<User>> GetUser(string username);
-		public Task<Result> EditUserData(ClaimsPrincipal currentUserPrincipal, UserEditDTO userDataDTO);
+		public Task<Result> EditUserData(ClaimsPrincipal currentUserPrincipal, UserEditDTO userDataDTO, CancellationToken cancellationToken);
 		public Task<Result> ChangeUsername(User currentUser, string newUsername);
 		public Task<Result> ChangeRealName(User currentUser, string? newRealName);
 		public Task<Result> ChangeBio(User currentUser, string? newBio);
-		public Task<Result> ChangeProfilePic(User currentUser, IFormFile newProfilePic);
+		public Task<Result> ChangeProfilePic(User currentUser, IFormFile newProfilePic, CancellationToken cancellationToken);
 
 		public Task<Result> FollowUser(ClaimsPrincipal currentUserPrincipal, string usernameToFollow);
 		public Task<Result> UnfollowUser(ClaimsPrincipal currentUserPrincipal, string usernameToUnfollow);
