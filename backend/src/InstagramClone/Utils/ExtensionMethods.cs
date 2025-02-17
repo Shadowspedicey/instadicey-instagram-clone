6e31a9bceb0c87c0f95de4e46b2bdef2055f5c80
Ahmed Khaled <shadowspedicey@gmail.com>
2025-02-17 03:39:48 +0200

    Backend - Fixed a bug in AppropriateResponseBasedOnResult
    It wasn't returning the correct response because i wasn't enum parsing the error code from the Metadata

--- backend/src/InstagramClone/Utils/ExtensionMethods.cs
+++ backend/src/InstagramClone/Utils/ExtensionMethods.cs
@@ -19,7 +19,7 @@ namespace InstagramClone.Utils
 				return controllerBase.ProblemWithErrors(statusCode: 400, errors: result.Errors.Select(e => e.Metadata));
 
 			IError error = result.Errors[0];
-			switch (error.Metadata["code"])
+			switch (Enum.Parse<ErrorCode>(error.Metadata["code"].ToString()!))
 			{
 				case ErrorCode.NotFound:
 					return controllerBase.ProblemWithErrors(statusCode: 404, detail: error.Message, errors: error.Metadata);
