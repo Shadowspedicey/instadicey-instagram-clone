704f20366fabee87715c5bc37ed6190191949617
Ahmed Khaled <shadowspedicey@gmail.com>
2025-02-17 13:50:18 +0200

    Backend - Added a CommentController

---
+++ backend/src/InstagramClone/Controllers/CommentController.cs
@@ -0,0 +1,39 @@
+﻿using InstagramClone.Data;
+using InstagramClone.Data.Entities;
+using InstagramClone.DTOs.Posts;
+using InstagramClone.Interfaces;
+using InstagramClone.Utils;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InstagramClone.Controllers
+{
+	[Route("[controller]")]
+	[Authorize]
+	[ApiController]
+	public class CommentController(AppDbContext dbContext, ICommentService commentService) : ControllerBase
+	{
+		private readonly AppDbContext _dbContext = dbContext;
+		private readonly ICommentService _commentService = commentService;
+
+		[HttpPost("post/{postID}")]
+		public async Task<IActionResult> AddPostComment(string postID, CommentCreateDTO commentDTO)
+		{
+			Post? post = await _dbContext.Posts.FindAsync(postID);
+			if (post is null)
+				return this.ProblemWithErrors(statusCode: 404, detail: "Post was not found.", errors: new[] { new CodedError(ErrorCode.NotFound, "Post was not found.") });
+
+			var result = await _commentService.AddComment(User, post, commentDTO.Comment);
+
+			return result.IsSuccess ? NoContent() : this.AppropriateResponseBasedOnResult(result);
+		}
+
+		[HttpPost("delete/{commentID}")]
+		public async Task<IActionResult> DeleteComment(string commentID)
+		{
+			var result = await _commentService.DeleteComment(User, commentID);
+
+			return result.IsSuccess ? NoContent() : this.AppropriateResponseBasedOnResult(result);
+		}
+	}
+}
---
+++ backend/src/InstagramClone/DTOs/Posts/CommentCreateDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InstagramClone.DTOs.Posts
+{
+	public class CommentCreateDTO
+	{
+		[StringLength(1000)]
+		public required string Comment { get; set; }
+	}
+}
