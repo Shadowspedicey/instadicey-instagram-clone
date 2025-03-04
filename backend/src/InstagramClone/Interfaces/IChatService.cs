a6bbb7e3a5b6bd24244c9c6332fe6504443d92f2
Ahmed Khaled <shadowspedicey@gmail.com>
2025-03-04 05:32:30 +0200

    Backend - Created an IChatService
    This will be used for TDD

---
+++ backend/src/InstagramClone/Interfaces/IChatService.cs
@@ -0,0 +1,15 @@
+﻿using FluentResults;
+using InstagramClone.Data.Entities;
+using System.Security.Claims;
+
+namespace InstagramClone.Interfaces
+{
+	public interface IChatService
+	{
+		public Task<Result<ChatRoom>> CreateChatRoom(ClaimsPrincipal currentUserPrinciple, string[] usernames);
+		public Task<Result<ChatRoom>> GetRoomWithUser(ClaimsPrincipal currentUserPrinciple, string username);
+		public Task<Result<ICollection<ChatRoom>>> GetUserRooms(ClaimsPrincipal currentUserPrinciple);
+		public Task<Result<ICollection<Message>>> GetMessages(ClaimsPrincipal currentUserPrinciple, string roomID);
+		public Task<Result<Message>> SendMessage(ClaimsPrincipal currentUserPrinciple, string roomID, string message);
+	}
+}
---
+++ backend/src/InstagramClone/Services/ChatService.cs
@@ -0,0 +1,40 @@
+﻿using FluentResults;
+using InstagramClone.Data;
+using InstagramClone.Data.Entities;
+using InstagramClone.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+
+namespace InstagramClone.Services
+{
+	public class ChatService(AppDbContext dbContext, IAuthorizationService authorizationService) : IChatService
+	{
+		private readonly AppDbContext _dbContext = dbContext;
+		private readonly IAuthorizationService _authorizationService = authorizationService;
+
+		public Task<Result<ChatRoom>> CreateChatRoom(ClaimsPrincipal currentUserPrinciple, string[] usernames)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<Result<ChatRoom>> GetRoomWithUser(ClaimsPrincipal currentUserPrinciple, string username)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<Result<ICollection<ChatRoom>>> GetUserRooms(ClaimsPrincipal currentUserPrinciple)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<Result<ICollection<Message>>> GetMessages(ClaimsPrincipal currentUserPrinciple, string roomID)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<Result<Message>> SendMessage(ClaimsPrincipal currentUserPrinciple, string roomID, string message)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
