91bd5ef71414d368cdef228591e22f8d1415252e
Ahmed Khaled <shadowspedicey@gmail.com>
2025-03-08 20:44:16 +0200

    Backend - Reversed the order of SortedMessages on ChatRooms
    So it can be displayed properly in frontend

--- backend/src/InstagramClone/Data/Entities/ChatRoom.cs
+++ backend/src/InstagramClone/Data/Entities/ChatRoom.cs
@@ -5,8 +5,8 @@
 		public string ID { get; set; } = Ulid.NewUlid().ToString();
 		public virtual ICollection<Message> Messages { get; set; } = [];
 		public virtual required ICollection<User> Users { get; set; } = [];
-		public IOrderedEnumerable<Message> SortedMessages => Messages.OrderByDescending(x => x.CreatedAt);
-		public Message? LastMessage => SortedMessages.FirstOrDefault();
+		public IOrderedEnumerable<Message> SortedMessages => Messages.OrderBy(x => x.CreatedAt);
+		public Message? LastMessage => SortedMessages.LastOrDefault();
 		public DateTime? LastUpdated => LastMessage?.CreatedAt;
 	}
 }
